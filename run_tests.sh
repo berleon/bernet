5354fbb9e0bb54230963f361b924a2f9da9aa5ef
berleon <github@leon-sixt.de>
2015-01-13 18:16:32 +0100

    add nose-timer to monitor execution time of the test suite

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,9 @@
 #! /usr/bin/env bash
 
-nosetests --with-coverage \
+nosetests \
+    --with-timer \
+    --timer-top-n 10 \
+    --with-coverage \
     --cover-package=bernet \
     --cover-html-dir=htmlcov \
     --cover-html \
