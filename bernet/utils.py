9bbb8381da9147905654c5dd6a1ecb0e5f5550de
berleon <github@leon-sixt.de>
2015-02-04 18:01:17 +0100

    Add `size` to get the size of a shape

--- bernet/utils.py
+++ bernet/utils.py
@@ -12,8 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import hashlib
+import operator
 
 import urllib.request
+from functools import reduce
 
 
 def download(url: str, file) -> bool:
@@ -78,3 +80,9 @@ def h(shape):
 def w(shape):
     """:return the width of `shape`."""
     return shape[-1]
+
+
+def size(shape):
+    """:return the total number of elements.
+    E.g. `size((2, 20, 10))` would be `2*20*10 = 400`"""
+    return reduce(operator.mul, shape, 1)
