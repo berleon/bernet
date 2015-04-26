190aecacb669b809e0a143945b20d0e849a3ec73
berleon <github@leon-sixt.de>
2015-04-26 18:18:56 +0200

    [pep8] removed empty line

--- test/test_utils.py
+++ test/test_utils.py
@@ -79,4 +79,3 @@ class TestUtils(TestCase):
         chunked = chunks(range(11), 4)
         self.assertListEqual(list(map(list, chunked)),
                              [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10]])
-
